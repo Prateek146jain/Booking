536265193deab434dcc71ea5c94d62aa7e439705
Prateek146jain <92501768+Prateek146jain@users.noreply.github.com>
2024-11-15 19:33:52 +0530

    Update Problems With Solutions.sql

--- Problems With Solutions.sql
+++ Problems With Solutions.sql
@@ -4,11 +4,11 @@
 
 -- 2. Find the average ride distance for each vehicle type:
 	
-    select Vehicle_Type,avg(Ride_Distance) from booking 
+    select Vehicle_Type,avg(Ride_Distance) as avg_distance from booking 
 				group by Vehicle_Type;
 
 -- 3. Get the total number of canceled rides by customers:
-	select count(*) from booking where Booking_status = 'Canceled by Customer';
+	select count(*) as total_count from booking where Booking_status = 'Canceled by Customer';
 
 -- 4.List the top 5 customers who booked the highest number of rides:
 	
@@ -16,7 +16,7 @@
  
 -- 5.Get the number of rides canceled by drivers due to personal and car-related issues:
 	
-    Select count(*) from booking where Canceled_Rides_by_Driver='Personal & Car related issue';
+    Select count(*) as total from booking where Canceled_Rides_by_Driver='Personal & Car related issue';
  
 -- 6.Find the maximum and minimum driver ratings for Prime Sedan bookings:
 	
@@ -28,11 +28,11 @@
         
 -- 8. Find the average customer rating per vehicle type:
 
-	Select Vehicle_Type, avg(Customer_rating) from booking group by Vehicle_Type;
+	Select Vehicle_Type, avg(Customer_rating) as avg_rating from booking group by Vehicle_Type;
 
 -- 9. Calculate the total booking value of rides completed successfully:
 
-	select sum(booking_value) from booking where Booking_Status='Success';
+	select sum(booking_value)as total_value from booking where Booking_Status='Success';
 
 -- 10. List all incomplete rides along with the reason:
 	
